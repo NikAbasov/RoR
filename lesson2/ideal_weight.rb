801ed626f4e07e04cc3391ec55972989a797e99d
NikAbasov <nik.abasov@gmail.com>
2018-01-30 12:32:30 +0300

    Исправил недочеты

--- lesson2/ideal_weight.rb
+++ lesson2/ideal_weight.rb
@@ -2,19 +2,17 @@ print "Hello! Please, enter your name?"
 name = gets.chomp
 
 print "Please, enter your growth? "
-growth = gets.to_i
+growth = gets.chomp.to_i
 
 print "Please, enter your weight? "
-weight = gets.to_i
+weight = gets.chomp.to_i
 
-ideal_weight = growth-110
+ideal_weight = growth - 110
 
-if 
-  ideal_weight >0
+ if  ideal_weight > 0
     puts "#{name}, your ideal weight is #{ideal_weight} "
 
-else
-  ideal_weight <0
+ else  ideal_weight < 0
     puts "#{name}, you need to eat unless=("
   
-  end
+ end
