85d36ae2147bff92724161ce758b5dd9ecda133f
NikAbasov <nik.abasov@gmail.com>
2018-02-22 14:25:37 +0300

    Update route.rb

--- lesson3/route.rb
+++ lesson3/route.rb
@@ -3,18 +3,18 @@ class Route
   attr_reader :stations
 
   def initialize(station_start, station_finish)
-    @stations = [station_start, station_finish]
+   @stations = [station_start, station_finish]
   end
 
   def add_station(station)
-    stations.insert(-2, station)
+   stations.insert(-2, station)
   end
 
   def remove_station(station)
-    stations.delete(station) unless [stations[0], stations[-1]].include? station
+   stations.delete(station) unless [stations[0], stations[-1]].include? station
   end
   
   def show_stations
-    print stations
+   @stations
   end
 end
