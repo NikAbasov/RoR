af4a3324bfbe93b0282565fc3e5fb33bf4c51f08
NikAbasov <nik.abasov@gmail.com>
2018-02-22 16:20:59 +0300

    Вернул код

--- lesson3/station.rb
+++ lesson3/station.rb
@@ -1,86 +1,26 @@
-class Train
-  attr_reader :speed, :wagons, :current_station_index
+class Station
 
-  def initialize(number,type, wagons)
-    @speed = 0
-    @number = number
-    @wagons = wagons
-    @type = type
-    @route = []
-    @current_station_index =[]
+  attr_reader :name, :trains
+
+  def initialize(name)
+    @name = name
+    @trains = []
   end
   
-  def up_speed(value)
-   @speed += value
+  def add_train(train)
+    @trains << train 
   end
 
-  def down_speed(value)
-    if value > @speed  then puts"no"
-    else
-    @speed -= value
-    end
-  end
-  def stop
-   @speed = 0
-  end
-  
-  def moving?
-    @speed > 0
+  def departure_train(train)
+    @trains.delete(train)
   end
 
-  def curent_speed
-   @speed
-  end
-  
-  def add_wagon
-   if moving? then print "Сначала остановите поезд"
-   else
-   @wagons += 1
-   end
+  def print_all_trains
+    @trains
   end
 
-  def remove_wagon
-   if moving? then print "Сначала остановите поезд"
-   else
-   @wagons -= 1 if wagons > 0
-   end
-  end
-      
-  def wagons_list
-   @wagons
-  end
-  
-   def take_route(route)
-    @route = route
-    @current_station_index = 0
-    self.current_station
+  def trains_by_type(type)
+    @trains.select { |train| train.type == type } 
   end
  
- def current_station
-  @route.stations[@current_station_index]
- end
-
- def previous_station
-  @route.stations[@current_station_index - 1] if @current_station_index > 0
- end
-
- def move_forward
-    unless next_station.nil?
-      current_station.departure_train(self)
-      next_station.add_train(self)
-      @current_station_index += 1 
-    end
- end
-
-  def move_back
-    unless previous_station.nil?
-      current_station.send_train(self)
-      previous_station.receive_train(self)
-      @current_station_index -= 1
-    end
-  end
-
- def next_station
-  @route.stations[@current_station_index + 1] if @route.stations.size > @current_station_index + 1
- end 
 end
